8a9f408116b9073a7fd61d70a83d4404c4fe6270
anshryan <ansharyan9999@gmail.com>
2025-06-22 19:06:47 +0530

    updte

---
+++ client/services/API.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const API=axios.create({baseURL:import.meta.env.VITE_URL,
+})
+
+API.interceptors.request.use((req)=>{
+  if(localStorage.getItem('token')){
+    req.headers.Authorization=`Bearer ${localStorage.getItem('token')}`
+  }
+  return req;
+});
+
+export default API;
