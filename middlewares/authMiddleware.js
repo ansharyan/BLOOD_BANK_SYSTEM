c7cfe6b7c2e8a95291da8838fe399ec2e76ddea6
anshryan <ansharyan9999@gmail.com>
2025-06-22 19:45:34 +0530

    final

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,6 +2,14 @@ const JWT=require('jsonwebtoken')
 
 module.exports=async(req,res,next)=>{
   try{
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).send({
+        success: false,
+        message: 'Authorization header missing or invalid',
+      });
+    }
     const token=req.headers['authorization'].split(" ")[1]
     JWT.verify(token,process.env.JWT_SECRET,(err,decode)=>{
       if(err){return res.status(401).send({
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path=require('path')
 const express=require('express');
 const dotenv=require('dotenv')
 const colors=require('colors')
@@ -9,29 +10,33 @@ const inventoryRoutes=require('./routes/inventoryRoutes')
 const analyticsRoutes=require('./routes/analyticsRoutes')
 const adminRoutes=require('./routes/adminRoutes')
 
-const path=require('path')
 dotenv.config()
+
 
 const app=express();
 connectDB();
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors());
 app.use(morgan('dev'))
 
 app.use("/api/v1/auth",auth)
 app.use("/api/v1/inventory",inventoryRoutes)
 app.use("/api/v1/analytics",analyticsRoutes)
 app.use("/api/v1/admin",adminRoutes)
+app.use("/api/v1", (req, res) => {
+    res.send("Welcome to Blood Bank System API");
+});
 
-if (process.env.NODE_ENV === 'production') {
+
+if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, './client/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/dist/index.html'));
+      res.sendFile(path.join(__dirname, './client/dist/index.html'));
   });
-}
 
+}
 const PORT=process.env.PORT||8080
 app.listen(PORT,(req,res)=>{
   console.log(`http://localhost:${PORT}`)
